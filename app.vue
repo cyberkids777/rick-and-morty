9c3317cc275cdbb4a537ed076a806ba22c66e645
Author Name <author@example.com>
2024-08-28 17:31:51 +0200

    Search is now available globally

--- app.vue
+++ app.vue
@@ -1,7 +1,13 @@
+<script setup>
+import { useCharacterStore } from '~/stores/characterStore';
+
+const store = useCharacterStore()
+
+</script>
+
 <template>
   <div>
+    <BaseSearch @on-search-submit="store.fetchCharacters" />
     <NuxtPage />
   </div>
 </template>
-<script setup lang="ts">
-</script>
