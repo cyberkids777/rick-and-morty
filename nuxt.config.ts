a757ab40d7f3ec5b344201bcb32452cca90a41aa
Author Name <author@example.com>
2024-08-31 13:17:29 +0200

    Configured UnoCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
     modules: [
-        '@nuxt/eslint', '@pinia/nuxt',
+        '@nuxt/eslint', '@pinia/nuxt', '@unocss/nuxt',
     ],
     ssr: false,
 })
